b111d948f3679a0b92f97e2a840012725d695dc0
61672 <rdeller.61672@farnborough.ac.uk>
2017-10-11 14:33:47 +0100

    Completed Main Template awaiting styling.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,10 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -34,15 +32,17 @@ public class Main extends Application {
         root.setLeft(leftPane);
         leftPane.setAlignment(Pos.CENTER_RIGHT);
         BorderPane.setAlignment(leftPane, Pos.CENTER_LEFT);
+        leftPane.setStyle("-fx-background-color: RED");
+        leftPane.setPrefWidth(400);
 
         VBox rightPane = new VBox(20);
         Button rightButton1 = new Button("Reaction Timer");
         rightPane.getChildren().add(rightButton1);
-        Button rightButton2 = new Button("Settings");
-        rightPane.getChildren().add(rightButton2);
-        root.setRight(rightPane);
+        rightPane.setStyle("-fx-background-color: aqua");
+        //BorderPane.setAlignment(rightPane, Pos.CENTER_RIGHT);
         rightPane.setAlignment(Pos.CENTER_LEFT);
-        BorderPane.setAlignment(rightPane, Pos.CENTER_RIGHT);
+        rightPane.setPrefWidth(400);
+        root.setRight(rightPane);
 
         VBox topPane = new VBox(20);
         Label Title = new Label("Welcome to my reaction Timer");
@@ -52,24 +52,45 @@ public class Main extends Application {
         topPane.setAlignment(Pos.CENTER);
         BorderPane.setAlignment(topPane, Pos.TOP_CENTER);
 
-        HBox bottomPane = new HBox(20);
+
+        HBox authorPane = new HBox(20);
+        authorPane.setPadding(new Insets(20));
+        authorPane.setStyle("-fx-background-color: Grey");
         Label Author = new Label("By Rory Deller");
         Author.setFont(new Font("Arial", 25));
-        bottomPane.getChildren().add(Author);
+        authorPane.getChildren().add(Author);
+
+        HBox quitSubPane = new HBox(20);
+        Button rightButton2 = new Button("Settings");
+        rightButton2.setPadding(new Insets(20));
+        quitSubPane.getChildren().add(rightButton2);
+
+        quitSubPane.setPadding(new Insets(20));
+        HBox.setHgrow(quitSubPane, Priority.ALWAYS);
+        quitSubPane.setAlignment(Pos.CENTER_RIGHT);
         Button bottomButton2 = new Button("Quit");
-        bottomPane.getChildren().add(bottomButton2);
-        root.setBottom(bottomPane);
-        bottomPane.setAlignment(Pos.BOTTOM_LEFT);
-        BorderPane.setAlignment(bottomPane, Pos.BOTTOM_CENTER);
+        bottomButton2.setPadding(new Insets(20));
+        quitSubPane.getChildren().add(bottomButton2);
+        quitSubPane.setAlignment(Pos.BOTTOM_RIGHT);
+        authorPane.getChildren().add(quitSubPane);
+        quitSubPane.setStyle("-fx-background-color: Silver");
+
+
+
+        root.setBottom(authorPane);
+
+
 
         VBox centerPane = new VBox(3);
         Button enterMusic = new Button("Musical Mode");
+        centerPane.setStyle("-fx-background-color: green");
         centerPane.getChildren().add(enterMusic);
+        centerPane.setPrefWidth(50);
 
         ImageView iv1 = new ImageView(new Image("Sample.gif"));
         centerPane.getChildren().add(iv1);
 
-        Button centerButton3 = new Button("");
+        Button centerButton3 = new Button("Statistics");
         centerPane.getChildren().add(centerButton3);
         root.setCenter(centerPane);
         centerPane.setAlignment(Pos.CENTER);
