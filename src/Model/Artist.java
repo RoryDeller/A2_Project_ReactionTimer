643f0f49a70e00b61bb4e3d2760bfcd32cedc7ac
61672 <rdeller.61672@farnborough.ac.uk>
2017-11-10 12:14:50 +0000

    Made one service class method, still many to go.

--- src/Model/Artist.java
+++ src/Model/Artist.java
@@ -3,14 +3,22 @@ package Model;
 public class Artist {
 
     private int artistID;
+    private int albumID;
     private String artistFirstname;
     private String artistSurname;
+    private String bandName;
 
-    public Artist(int artistID, String artistFirstname, String artistSurname) {
+
+
+    public Artist(int artistID, int albumID, String artistFirstname, String artistSurname, String bandName) {
         this.artistID = artistID;
+        this.albumID = albumID;
         this.artistFirstname = artistFirstname;
         this.artistSurname = artistSurname;
+        this.bandName = bandName;
     }
+
+
 
     public int getArtistID() {
         return artistID;
@@ -19,6 +27,10 @@ public class Artist {
     public void setArtistID(int artistID) {
         this.artistID = artistID;
     }
+
+    public int getAlbumID() {return albumID;}
+
+    public void setAlbumID(int albumID) {this.albumID = albumID;}
 
     public String getArtistFirstname() {
         return artistFirstname;
@@ -35,4 +47,8 @@ public class Artist {
     public void setArtistSurname(String artistSurname) {
         this.artistSurname = artistSurname;
     }
+
+    public String getBandName() {return bandName;}
+
+    public void setBandName(String bandName) {this.bandName = bandName;}
 }
--- src/Model/ArtistService.java
+++ src/Model/ArtistService.java
@@ -1,10 +1,32 @@
 package Model;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ArtistService {
 
-    public static void selectAll(List<Artist> destination, DatabaseConnection database){}
+    public static void selectAll(List<Artist> targetList, DatabaseConnection database){
+
+        PreparedStatement statement = database.newStatement("SELECT artistID, albumID, artistFirstname, artistSurname, bandName FROM Artists ORDER BY artistID");
+
+        try {
+            if (statement != null) {
+
+                ResultSet results = database.runQuery(statement);
+
+                if (results != null) {
+                    while (results.next()) {
+                        targetList.add(new Artist(results.getInt("artistID"), results.getInt("albumID"), results.getString("artistFirstname"), results.getString("artistSurname"), results.getString("bandName")));
+                    }
+                }
+            }
+        } catch (SQLException resultsException) {
+            System.out.println("Database select all error: " + resultsException.getMessage());
+        }
+
+    }
     //public static Artist selectByID(int id, DatabaseConnection database){}
     //public static void save(Artist artist, DatabaseConnection database){} //Insert and Update
     //public static void deleteByID(int id, DatabaseConnection database){}
